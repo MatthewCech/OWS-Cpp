8fbe59bf4371e767f608dcccf4830770024a02d9
Matthew <itsmatthewcech@gmail.com>
2017-10-20 18:11:52 -0700

    = Updated build system and file connection

--- Build/premake5.lua
+++ Build/premake5.lua
@@ -1,36 +1,30 @@
 -- Premake5 Wiki: https://github.com/premake/premake-core/wiki
--- Based on Floofs_Inc Premake GLFW demo courtesy of JohannesMP
+-- Based on Premake GLFW demo courtesy of JohannesMP
+-- https://github.com/JohannesMP
 
 
--- Variable definition
+-- Variable definition: CHANGE THESE TO MODIFY PROJECT NAME
 -- Brief reminder: This is actual lua, functions and whatnot are totally allowed.
-local ROOT = "../"   -- Path to project root
+local ROOT         = "../"          -- Path to project root
 
----------------------------------
--- [ WORKSPACE CONFIGURATION   --
----------------------------------
-workspace "OneWaySync"                -- Solution Name
-  configurations { "Debug", "Release"}     -- Optimization/General config mode in VS
-  platforms { "x64", "x86"}                -- Dropdown platforms section in VS
-  location (ROOT .. "project_" .. _ACTION) -- Note: _ACTION is the argument passed to premake.
 
-  -------------------------------
-  -- [ PROJECT CONFIGURATION ] --
-  ------------------------------- 
-  project "OneWaySync"        -- Project name
-    targetname "onewaysync"  -- Executable name
-    kind "ConsoleApp"     -- Style of app in project- WindowedApp, ConsoleApp, etc.
-    language "C++"
+-- [ WORKSPACE CONFIGURATION ] --
+workspace "Boilerplate"                      -- Solution Name
+    configurations { "Debug", "Release"}     -- Optimization/General config mode in VS
+    platforms { "x64", "x86"}                -- Dropdown platforms section in VS
+    location (ROOT .. "project_" .. _ACTION) -- Note: _ACTION is the argument passed to premake.
 
-  -------------------------------
-  -- [ COMPILER/LINKER CONFIG] --
-  -------------------------------
-  
-  -- Set compiler flags
-  flags "FatalWarnings"  -- Warnings to 11! (all warnings on)
+    -- [ PROJECT CONFIGURATION ] --
+    project "boilerplate"        -- Project name
+        targetname "boilerplate" -- Executable name
+        kind "ConsoleApp"        -- Style of app in project- WindowedApp, ConsoleApp, etc.
+        language "C++"
 
-    -- Generate filters with info provided, VS
-  filter { "platforms:*86" }
+    -- [ COMPILER/LINKER CONFIG] --
+    flags "FatalWarnings"  -- Warnings to 11! (all warnings on)
+
+    -- Generate filters with info provided for Visual Studio
+    filter { "platforms:*86" }
       architecture "x86"
     filter { "platforms:*64" }
       architecture "x64"
@@ -38,21 +32,17 @@ workspace "OneWaySync"                -- Solution Name
     -- Generate configs dropdown info, VS
     filter { "configurations:Debug" }
       defines { "DEBUG" }  -- Actively defined in code, can be checked for.
-      symbols "On"
+      flags { "Symbols" }
     filter { "configurations:Release" }
       defines { "NDEBUG" } -- Actively defined in code, can be checked for.
       optimize "On"
 
-    -- Reset filter.
-    filter {}
+    filter {} -- Reset filter.
 
-
-  ------------------------------
-  -- [ BUILD CONFIGURATIONS ] --
-  ------------------------------
+    -- [ BUILD CONFIGURATIONS ] --
     local cur_toolset = "default" -- workaround for premake issue #257
 
-    filter {"system:macosx" }           -- Mac uses clang.
+    filter {"system:macosx" } -- Mac uses clang.
       toolset "clang"
       cur_toolset = "clang"
    
@@ -61,7 +51,7 @@ workspace "OneWaySync"                -- Solution Name
 
     -- Set the rpath on the executable, to allow for relative path for dynamic lib
     filter { "system:macosx" }
-      if cur_toolset == "clang" or cur_toolset == "gcc" then  -- issue #257
+      if cur_toolset == "clang" or cur_toolset == "gcc" then  
         linkoptions { "-rpath @executable_path/lib" }
       end
     
@@ -70,63 +60,61 @@ workspace "OneWaySync"                -- Solution Name
 
     filter {} -- clear filter   
 
-    ----------------------------------
+
     -- [ FILE PATH CONFIGURATIONS ] --
-    ----------------------------------
     local output_dir_root         = ROOT .. "bin_%{cfg.platform}_%{cfg.buildcfg}_" .. _ACTION
-    targetdir(output_dir_root )    -- Where all output files are stored
     local output_dir_lib          = output_dir_root .. "/libs" -- Mac Specific
-
-    local source_dir_engine       = ROOT .. "Source"
-    local source_dir_external     = ROOT .. "External"
-
-    local source_dir_includes     = source_dir_external .. "/**/Includes"
-    local source_dir_libs         = source_dir_external .. "/**/" .. "Libs_" .. os.get()
-    -- optional for libs that are 32 or 64 bit specific
-    local source_dir_libs32       = source_dir_libs .. "/lib_x32"
-    local source_dir_libs64       = source_dir_libs .. "/lib_x64"
-
+    targetdir(output_dir_root)    -- Where all output files are stored
+    local source_dir_root         = ROOT .. "Source"
+    local source_dir_includes     = ROOT .. "External" .. "/**/Includes"
+    local source_dir_libs         = ROOT .. "External" .. "/**/" .. "Libs_" .. os.target()
 
     -- Files to be compiled (cpp) or added to project (visual studio)
     files
     {
-      source_dir_engine .. "/**.cpp",
-      source_dir_engine .. "/**.hpp",
-      source_dir_engine .. "/**.tpp",
+      source_dir_root .. "/**.c",
+      source_dir_root .. "/**.h",
+      source_dir_root .. "/**.cpp",
+      source_dir_root .. "/**.hpp",
+      source_dir_root .. "/**.tpp",
     }
 
-    -- Don't compile the template implementations!
+    -- Omit templates from visual studio
     filter { "files:**.tpp" }
       flags {"ExcludeFromBuild"}
     filter {}
 
     -- Ignore files for other operating systems (not necessary in this project)
-    filter { "system:macosx" } removefiles { source_dir_engine .. "/**_windows.*" }
-    filter { "system:windows" } removefiles { source_dir_engine .. "/**_macosx.*"  }
+    filter { "system:macosx" } removefiles { source_dir_root .. "/**_windows.*" }
+    filter { "system:windows" } removefiles { source_dir_root .. "/**_macosx.*"  }
     filter {} -- reset filter
 
-    -- Filter config in visual studio
+    -- Organize VS filters
     vpaths {
       ["Headers"] = 
           { 
-            source_dir_engine .. "**.h", 
-            source_dir_engine .. "**.hpp"
+            source_dir_root .. "**.h", 
+            source_dir_root .. "**.hpp"
           },
       ["Sources/*"] = 
         {
-          source_dir_engine .. "**.c", 
-          source_dir_engine .. "**.cpp", 
-          source_dir_engine .. "**.tpp"
+          source_dir_root .. "**.c", 
+          source_dir_root .. "**.cpp", 
+          source_dir_root .. "**.tpp"
         },
       ["Docs"] = "**.txt"
     }
 
+    -- Where compiler should look for library includes
+    -- NOTE: for library headers always use  '#include <LIB/lib.hpp>' not quotes
+    -- The LIB folder is an additional step to be added manually, creating an effective namespace.
+    -- This prevents silly name overlapt, which we don't want to happen.
     includedirs
     {
-      source_dir_engine, -- for absolute inclides
       source_dir_includes,
       source_dir_root
     }
+    
 
     -- Where linker should look for library files
     -- NOTE: currently each library must have "LIBS_<OS>" in its path.
@@ -139,8 +127,9 @@ workspace "OneWaySync"                -- Solution Name
       source_dir_libs .. "/lib_%{cfg.platform}/%{cfg.buildcfg}"  -- libs with BOTH x32/x64 AND Release/Debug versions (order reversed)
     }
 
-    -- OS-specific Libraries - Dynamic libs will need to be copied to output
-
+    -- OS-specific Libraries - Dynamic libs will need to be copied to output.
+    -- These are the configurations for windows and mac specifically, 
+    -- more can be added for linux.
     filter { "system:windows" }  -- Currently all static libs; No copying
       links
       {
